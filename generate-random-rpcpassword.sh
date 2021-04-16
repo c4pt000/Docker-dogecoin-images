ae5ae0b4f84d1f268bcd7420e4cadab3029bad19
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-04-16 17:09:15 -0400

    Update generate-random-rpcpassword.sh

--- generate-random-rpcpassword.sh
+++ generate-random-rpcpassword.sh
@@ -10,9 +10,21 @@ rpcuser=rpcuser
 prune=2200
 mempoolexpiry=24
 dbcache=1000
-server=1
+daemon=1
 port=22556
 rpcport=22555
+
+
+
+addnode=95.85.29.144
+addnode=162.243.113.110
+addnode=146.185.181.114
+addnode=188.165.19.28
+addnode=166.78.155.36
+addnode=doge.netcodepool.org
+addnode=doge.cryptoculture.net
+addnode=dogepool.pw
+addnode=78.46.57.132
 ' >> /root/.dogecoin/dogecoin.conf
 sleep 5
 dogecoind -daemon
