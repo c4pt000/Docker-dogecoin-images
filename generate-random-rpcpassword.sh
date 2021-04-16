0f482ecbbd77fd68649742df9ea8d76673773084
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-04-16 17:13:07 -0400

    Update generate-random-rpcpassword.sh

--- generate-random-rpcpassword.sh
+++ generate-random-rpcpassword.sh
@@ -1,6 +1,6 @@
 # npm -g install random-string
 
-random-string > random.txt
+randomstring > random.txt
 sed -e 's/^/rpcpassword=/' random.txt > new_random.txt
 sed -n 1p new_random.txt > rpcpassword.txt
 rm new_random.txt
